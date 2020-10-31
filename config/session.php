1f879734af388b63b120c70caf5c3abe0a7f8669
Veecthorpaul <52431939+Veecthorpaul@users.noreply.github.com>
2020-10-31 15:37:15 +0100

    Update session.php

--- config/session.php
+++ config/session.php
@@ -29,10 +29,10 @@ return [
     | to be allowed to remain idle before it expires. If you want them
     | to immediately expire on the browser closing, set that option.
     |
-    */
+    
+    'lifetime' => env('SESSION_LIFETIME', 120), */
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
-
+    'lifetime' => 1 * (60 * 24 * 365),
     'expire_on_close' => false,
 
     /*
